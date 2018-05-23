923ae6139f0d5731a9e2a7ded5c1d5d24ade6ed9
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-05-23 11:06:06 +0530

    implemented CryptoNewsAdapter

--- app/src/main/java/com/example/rohantaneja/hv_mockup/adapter/CryptoNewsAdapter.java
+++ app/src/main/java/com/example/rohantaneja/hv_mockup/adapter/CryptoNewsAdapter.java
@@ -7,41 +7,42 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.rohantaneja.hv_mockup.R;
-import com.example.rohantaneja.hv_mockup.adapter.viewholder.CryptoDetailsViewHolder;
+import com.example.rohantaneja.hv_mockup.adapter.viewholder.CryptoNewsViewHolder;
 import com.example.rohantaneja.hv_mockup.model.CryptoDetails;
+import com.example.rohantaneja.hv_mockup.model.CryptoNews;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CryptoDetailsAdapter extends RecyclerView.Adapter<CryptoDetailsViewHolder> {
+public class CryptoNewsAdapter extends RecyclerView.Adapter<CryptoNewsViewHolder> {
 
     private final Context mContext;
-    private List<CryptoDetails> mCryptoDetailsList;
+    private List<CryptoNews> mCryptoNewsList;
 
-    public CryptoDetailsAdapter(Context context) {
+    public CryptoNewsAdapter(Context context) {
         mContext = context;
-        mCryptoDetailsList = new ArrayList<>();
+        mCryptoNewsList = new ArrayList<>();
     }
 
-    public void updateList(List<CryptoDetails> itemsList) {
-        mCryptoDetailsList = itemsList;
+    public void updateList(List<CryptoNews> itemsList) {
+        mCryptoNewsList = itemsList;
         notifyDataSetChanged();
     }
 
     @Override
-    public CryptoDetailsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_crypto_details, parent, false);
-        return new CryptoDetailsViewHolder(mContext, v);
+    public CryptoNewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_crypto_news, parent, false);
+        return new CryptoNewsViewHolder(mContext, v);
     }
 
     @Override
-    public void onBindViewHolder(CryptoDetailsViewHolder holder, int position) {
-        CryptoDetails item = mCryptoDetailsList.get(position);
+    public void onBindViewHolder(CryptoNewsViewHolder holder, int position) {
+        CryptoNews item = mCryptoNewsList.get(position);
         holder.bindData(item);
     }
 
     @Override
     public int getItemCount() {
-        return mCryptoDetailsList == null ? 0 : mCryptoDetailsList.size();
+        return mCryptoNewsList == null ? 0 : mCryptoNewsList.size();
     }
 }
